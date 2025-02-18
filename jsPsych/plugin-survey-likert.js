3c940eed92d30e379bf26780e7f3b3a787c3721e
kushinm <kushinm11@gmail.com>
2025-02-18 17:11:38 -0600

    add missing plugins

--- jsPsych/plugin-survey-likert.js
+++ jsPsych/plugin-survey-likert.js
@@ -97,7 +97,7 @@ var jsPsychSurveyLikert = (function (jspsych) {
                   w +
                   "; margin:auto; padding:0 0 35px; display:block; font-size: 14px; line-height:1.1em; }" +
                   ".jspsych-survey-likert-opt-label { line-height: 1.1em; color: #444; }" +
-                  ".jspsych-survey-likert-opts:before { content: ''; position:relative; top:11px; /*left:9.5%;*/ display:block; background-color:#efefef; height:4px; width:100%; }" +
+                //   ".jspsych-survey-likert-opts:before { content: ''; position:relative; top:11px; /*left:9.5%;*/ display:block; background-color:#efefef; height:4px; width:100%; }" +
                   ".jspsych-survey-likert-opts:last-of-type { border-bottom: 0; }" +
                   ".jspsych-survey-likert-opts li { display:inline-block; /*width:19%;*/ text-align:center; vertical-align: top; }" +
                   ".jspsych-survey-likert-opts li input[type=radio] { display:block; position:relative; top:0; left:50%; margin-left:-6px; }";
